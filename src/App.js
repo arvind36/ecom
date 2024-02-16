9af65ddeeb13110145297e30de15dc3dba6ce6d4
arvind <arvindsharan46@gmail.com>
2024-02-16 17:27:40 +0530

    done with add to cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Layout from './Layout'
+import Home from './pages/Home'
+import Clothes from './pages/Clothes'
+import Electronics from './pages/Electronics'
+import Cart from './pages/Cart'
+import ProductDetail from './pages/ProductDetails'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>} >
+          <Route index element={<Home/>} />
+          <Route path='/clothes' element={<Clothes/>} />
+          <Route path='/product-detail/:product_id' element={<ProductDetail/>} />
+          <Route path='/electronics' element={<Electronics/>} />
+          <Route path='/cart' element={<Cart/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
